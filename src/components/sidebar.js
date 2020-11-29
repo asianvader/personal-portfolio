2b11fb8c6ed47b5114f3cec8206edf8ff9aaac8f
asianvader <phoebe.voong@gmail.com>
2020-11-29 01:26:28 +0000

    Add sidebar component. Add image and fontawesome icons.

---
+++ src/components/sidebar.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faGithubSquare,
+  faLinkedin,
+  faTwitterSquare,
+} from "@fortawesome/free-brands-svg-icons"
+import Phoebe from "../images/phoebe.jpg"
+import "fontsource-roboto"
+import "fontsource-open-sans"
+
+function Sidebar() {
+  const twitterIcon = <FontAwesomeIcon icon={faTwitterSquare} />
+  const githubIcon = <FontAwesomeIcon icon={faGithubSquare} />
+  const linkedinIcon = <FontAwesomeIcon icon={faLinkedin} />
+
+  return (
+    <div>
+      <img src={Phoebe} alt="Phoebe smiling" />
+      <h2>
+        Hello. <br /> I'm Phoebe Voong-Fadel. I'm a front-end developer.
+      </h2>
+      {twitterIcon}
+      {githubIcon}
+      {linkedinIcon}
+    </div>
+  )
+}
+
+export default Sidebar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react"
+import Sidebar from "../components/sidebar"
 
 export default function Home() {
   return (
     <div>
-      Hello world
+      <Sidebar />
     </div>
   )
 }
