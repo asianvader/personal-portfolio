9fc54012e47b9974d26d54838b2faa690f09e7f3
asianvader <phoebe.voong@gmail.com>
2020-11-30 15:29:09 +0000

    Right align image, buttons and h2 of sidebar.

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -11,43 +11,65 @@ import "fontsource-roboto"
 import "fontsource-open-sans"
 
 function Sidebar() {
-  const twitterIcon = <FontAwesomeIcon icon={faTwitterSquare} className={sidebarStyles.fontAwesome} size="3x" />
-  const githubIcon = <FontAwesomeIcon icon={faGithubSquare} className={sidebarStyles.fontAwesome} size="3x" />
-  const linkedinIcon = <FontAwesomeIcon icon={faLinkedin} className={sidebarStyles.fontAwesome} size="3x" />
+  const twitterIcon = (
+    <FontAwesomeIcon
+      icon={faTwitterSquare}
+      className={sidebarStyles.fontAwesome}
+      size="3x"
+    />
+  )
+  const githubIcon = (
+    <FontAwesomeIcon
+      icon={faGithubSquare}
+      className={sidebarStyles.fontAwesome}
+      size="3x"
+    />
+  )
+  const linkedinIcon = (
+    <FontAwesomeIcon
+      icon={faLinkedin}
+      className={sidebarStyles.fontAwesome}
+      size="3x"
+    />
+  )
 
   return (
     <div>
-      <img src={Phoebe} alt="Phoebe smiling" />
+      <div className={sidebarStyles.imgPhoebe}>
+        <img src={Phoebe} alt="Phoebe smiling" />
+      </div>
       <h2>
         Hello. <br /> I'm Phoebe Voong-Fadel. I'm a front-end developer.
       </h2>
-      <button class="btn" aria-label="Twitter link">
-        <a
-          href="https://twitter.com/PhoebeVF"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          {twitterIcon}
-        </a>
-      </button>
-      <button class="btn" aria-label="Github link">
-        <a
-          href="https://github.com/asianvader"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          {githubIcon}
-        </a>
-      </button>
-      <button class="btn" aria-label="LinkedIn link">
-        <a
-          href="https://www.linkedin.com/in/phoebe-voong-fadel-36961234/"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          {linkedinIcon}
-        </a>
-      </button>
+      <div className={sidebarStyles.buttonContainer}>
+        <button class="btn" aria-label="Twitter link">
+          <a
+            href="https://twitter.com/PhoebeVF"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            {twitterIcon}
+          </a>
+        </button>
+        <button class="btn" aria-label="Github link">
+          <a
+            href="https://github.com/asianvader"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            {githubIcon}
+          </a>
+        </button>
+        <button class="btn" aria-label="LinkedIn link">
+          <a
+            href="https://www.linkedin.com/in/phoebe-voong-fadel-36961234/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            {linkedinIcon}
+          </a>
+        </button>
+      </div>
     </div>
   )
 }
