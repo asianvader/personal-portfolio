be4c6059ea63324baaecf01cdbcf6586d52d1916
asianvader <phoebe.voong@gmail.com>
2020-12-01 23:11:43 +0000

    Add gatsby-image package

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,16 @@
+const path = require(`path`)
+
 module.exports = {
-  plugins: [`gatsby-plugin-fontawesome-css`],
+  plugins: [
+    `gatsby-plugin-fontawesome-css`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 }
