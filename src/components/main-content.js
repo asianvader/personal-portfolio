c8116ca5a0cf0e7ea8891c57bc3694a0574d6183
asianvader <phoebe.voong@gmail.com>
2020-12-03 14:35:38 +0000

    Change styling of portfolio cards

--- src/components/main-content.js
+++ src/components/main-content.js
@@ -4,7 +4,6 @@ import { graphql, useStaticQuery } from "gatsby"
 import BackgroundImage from "gatsby-background-image"
 import PortfolioCard from "./portfolio-card"
 
-
 const MainContent = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -37,7 +36,10 @@ const MainContent = () => {
         <section id="introduction" className={mainContentStyles.introduction}>
           <div className={mainContentStyles.textContainer}>
             <h1 className={mainContentStyles.introHeader}>
-              Hello, <span className={mainContentStyles.textPurple}>my name is Phoebe.</span>{" "}
+              Hello,{" "}
+              <span className={mainContentStyles.textPurple}>
+                my name is Phoebe.
+              </span>{" "}
             </h1>
             <p>
               I'm a front-end developer and I build interactive maps for a
@@ -48,62 +50,65 @@ const MainContent = () => {
         </section>
 
         <section id="about">
-        <div className={mainContentStyles.textContainer}>
-          <h2 className={mainContentStyles.aboutHeader}>About me</h2>
-          <p>
-            I was born and educated in the U.K. Before entering the world of web
-            development, I worked for over a decade at various universities in
-            London. Throughout my career in Higher Education, I was a strong advocate of using technology and software to
-            automate repetitive administrative tasks. So I decided to pursue a more
-            technical career.
-          </p>
-          <p>
-            Apart from my passion for learning the latest frontend technologies
-            and frameworks, I also write for{" "}
-            <a
-              href="https://www.freecodecamp.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              freeCodeCamp.org
-            </a>
-            . You can read my articles{" "}
-            <a
-              href="https://www.freecodecamp.org/news/author/phoebe"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              here
-            </a>
-            .{" "}
-          </p>
-          <p>
-            In my spare time, I mentor early-career developers for{" "}
-            <a
-              href="https://the-collab-lab.codes/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              The Collab Lab
-            </a>
-            . The Collab Lab is a non-profit and their goal is to help
-            under-represented folks break into the tech industry.
-          </p>
-          <p>
-            Aside from work, my two young children keep me occupied. But
-            sometimes I manage to bake a cake or two during my spare time.
-          </p>
+          <div className={mainContentStyles.textContainer}>
+            <h2 className={mainContentStyles.aboutHeader}>About me</h2>
+            <p>
+              I was born and educated in the U.K. Before entering the world of
+              web development, I worked for over a decade at various
+              universities in London. Throughout my career in Higher Education,
+              I was a strong advocate of using technology and software to
+              automate repetitive administrative tasks. So I decided to pursue a
+              more technical career.
+            </p>
+            <p>
+              Apart from my passion for learning the latest frontend
+              technologies and frameworks, I also write for{" "}
+              <a
+                href="https://www.freecodecamp.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                freeCodeCamp.org
+              </a>
+              . You can read my articles{" "}
+              <a
+                href="https://www.freecodecamp.org/news/author/phoebe"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                here
+              </a>
+              .{" "}
+            </p>
+            <p>
+              In my spare time, I mentor early-career developers for{" "}
+              <a
+                href="https://the-collab-lab.codes/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                The Collab Lab
+              </a>
+              . The Collab Lab is a non-profit and their goal is to help
+              under-represented folks break into the tech industry.
+            </p>
+            <p>
+              Aside from work, my two young children keep me occupied. But
+              sometimes I manage to bake a cake or two during my spare time.
+            </p>
           </div>
         </section>
         <section id="wordpress-portfolio">
-        <div className={mainContentStyles.textContainer}>
-          <h2 className={mainContentStyles.freelanceHeader}>Freelance projects</h2>
-          <PortfolioCard
-            image={data.srilicious.childImageSharp.fluid}
-            title="Sri-Licious"
-            excerpt="A local food business wanted a complete redesign of their website"
-            link="https://srilicious.co.uk/"
-          />
+          <div className={mainContentStyles.textContainer}>
+            <h2 className={mainContentStyles.freelanceHeader}>
+              Freelance projects
+            </h2>
+            <PortfolioCard
+              image={data.srilicious.childImageSharp.fluid}
+              title="Sri-Licious"
+              excerpt="A multi-page marketing website. I did a complete redesign of Sri-Licious' website to bring it line with their updated branding."
+              link="https://srilicious.co.uk/"
+            />
           </div>
         </section>
       </main>
--- src/components/portfolio-card.js
+++ src/components/portfolio-card.js
@@ -1,19 +1,19 @@
 import React from "react"
-import portfolioCardStyles from "./portfoliocard.module.css" 
+import portfolioCardStyles from "./portfoliocard.module.css"
 import Img from "gatsby-image"
 
-function PortfolioCard({ title, excerpt, link, image }) {
+function PortfolioCard({ title, excerpt, link, image, background }) {
   return (
-    <div className={portfolioCardStyles.wrapper}>
+      <div className={portfolioCardStyles.wrapper}>
         <Img fluid={image} className={portfolioCardStyles.cardImage} />
-      <h3 className={portfolioCardStyles.cardTitle}>{title}</h3>
-      <p className={portfolioCardStyles.cardExcerpt}>{excerpt}</p>
-      <button role="link" className={portfolioCardStyles.cardButton}>
-        <a href={link} rel="noopener noreferrer" target="_blank">
-          Live Site
-        </a>
-      </button>
-    </div>
+        <h3 className={portfolioCardStyles.cardTitle}>{title}</h3>
+        <p className={portfolioCardStyles.cardExcerpt}>{excerpt}</p>
+        <button role="link" className={portfolioCardStyles.cardButton}>
+          <a href={link} rel="noopener noreferrer" target="_blank">
+            Live Site
+          </a>
+        </button>
+      </div>
   )
 }
 
