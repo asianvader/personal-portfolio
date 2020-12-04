1d2e6d0d2b61af425771d73079b7ae32fabd1149
asianvader <phoebe.voong@gmail.com>
2020-12-04 10:22:27 +0000

    Revert "Revert: Add react-helmet to index.js"
    This reverts commit 88def303cb59f9d44acb37508216db17b5b25506.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,13 @@
 import React from "react"
 import Layout from "../components/layout"
 import MainContent from "../components/main-content"
-import { Helmet } from "react-helmet"
 
 export default function Home() {
   return (
-    <Layout>
-      <Helmet>
-        <title>Phoebe Voong-Fadel | Front-end Developer</title>
-        <meta name="description" content="Phoebe Voong-Fadel. Front-end Developer" />
-      </Helmet>
-      <MainContent />
-    </Layout>
+    <div>
+      <Layout>
+        <MainContent />
+      </Layout>
+    </div>
   )
 }
