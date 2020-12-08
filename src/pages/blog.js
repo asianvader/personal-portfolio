b9efa85d702642dc59ff2364c0f883fd9c09fd02
asianvader <phoebe.voong@gmail.com>
2020-12-08 11:26:00 +0000

    Add title and description metadata to blog page.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,21 @@
 import React from "react"
 import Layout from "../components/layout"
 import BlogContent from "../components/blog-content"
-
+import useSiteMetadata from "../components/useSiteMetadata"
+import { Helmet } from "react-helmet"
 
 export default function Blog() {
-    return (
-        <Layout>
-             <BlogContent />
-        </Layout>
-       
-    )
+  const { title, description } = useSiteMetadata()
+  return (
+    <div>
+      <Helmet>
+        <title>{`${title} | Articles`}</title>
+        <link rel="canonical" href="https://thecodinghamster.com" />
+        <meta name="description" content={description} />
+      </Helmet>
+      <Layout>
+        <BlogContent />
+      </Layout>
+    </div>
+  )
 }
